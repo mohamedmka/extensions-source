9e96b0caffc0ac873073f722fe8c18ffc335a53d
mohamedmka <hachimiabedelkader2@gmail.com>
2026-07-15 09:04:31 +0100

    Update build.gradle.kts

--- src/ar/mangatek/build.gradle.kts
+++ src/ar/mangatek/build.gradle.kts
@@ -1,12 +1,11 @@
 import io.github.keiyoushi.gradle.api.ContentWarning
-
 plugins {
     alias(kei.plugins.extension)
 }
 
 keiyoushi {
     name = "MangaTek"
-    versionCode = 3
+    versionCode = 4
     contentWarning = ContentWarning.SAFE
     libVersion = "1.4"
 
