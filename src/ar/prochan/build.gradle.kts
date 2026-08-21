d821a0a1a6cf4667e55e921744add457a182a0ee
mohamedmka <hachimiabedelkader2@gmail.com>
2026-08-21 21:27:36 +0100

    Add build.gradle.kts for Prochan extension

---
+++ src/ar/prochan/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("plugin.serialization")
+}
+
+android {
+    compileSdk = 34
+    namespace = "eu.kanade.tachiyomi.extension.ar.prochan"
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+
+        buildConfigField("String", "EXTENSION_NAME", "\"Prochan\"")
+        buildConfigField("String", "EXTENSION_LANG", "\"ar\"")
+        buildConfigField("String", "EXTENSION_CLASS", "\"eu.kanade.tachiyomi.extension.ar.prochan.Prochan\"")
+        buildConfigField("int", "EXTENSION_VERSION_CODE", "1")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Tachiyomi Core
+    compileOnly(project(":core"))
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.9.0")
+
+    // Jsoup
+    implementation("org.jsoup:jsoup:1.15.3")
+
+    // AndroidX
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+
+    // Ktor
+    implementation("io.ktor:ktor-client-okhttp:2.3.4")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
