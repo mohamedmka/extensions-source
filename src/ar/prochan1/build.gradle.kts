3678fb606c21c5333895f41d00ea736dee2267eb
mohamedmka <hachimiabedelkader2@gmail.com>
2026-08-21 22:58:50 +0100

    Update build.gradle.kts

--- src/ar/prochan1/build.gradle.kts
+++ src/ar/prochan1/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 
 keiyoushi {
     name = "ProChan"
-    versionCode = 5
+    versionCode = 0
     contentWarning = ContentWarning.SAFE
-    libVersion = "1.6"
+    libVersion = "1.4"
 
     source {
         lang = "ar"
